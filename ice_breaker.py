2661d645137b45637e15539ceef82858afd7ad4e
michaelallch <allouche.m95@gmail.com>
2023-09-28 17:36:35 +0200

    changes twitter

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,22 +5,20 @@ from langchain.chains import LLMChain
 from third_parties.linkedin import scrape_linkedin_profiles
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-information = """
-Elon Musk (prononcé en anglais : /ˈiː.lɒn ˈmʌsk/), né le 28 juin 1971 à Pretoria (Afrique du Sud), est un entrepreneur, chef d'entreprises et milliardaire sud-africain, canadien et américain.
+from third_parties.twitter import scrape_user_tweets
+from agents.linkedin_lookup_agent import lookup as twitter_lookup_agent
 
-Il est cofondateur et président-directeur général de la société astronautique SpaceX et directeur général de la société automobile Tesla. En janvier 2021, selon Bloomberg, Elon Musk devient, à 49 ans, l'homme le plus riche du monde, avec une fortune estimée à plus de 192,3 milliards de dollars. En octobre 2022, il devient le propriétaire de Twitter, par un achat à 44 milliards de dollars.
 
-Il commence sa carrière dans les affaires, comme cofondateur de la société de logiciels Zip2 avec son frère, Kimbal. La start-up est acquise par Compaq pour 307 millions de dollars en 1999. La même année, Musk cofonde la banque en ligne X.com, qui fusionne avec Confinity en 2000 pour former PayPal. eBay rachète PayPal en 2002 pour 1,5 milliard de dollars.
-
-En 2002, Musk fonde SpaceX, un fabricant aérospatial et une société de services de transport spatial, et en est le PDG. En 2004, il investit 6,5 milliards de dollars dans le constructeur de véhicules électriques Tesla, en devient le premier actionnaire et intègre son conseil d'administration, avant de prendre le poste de PDG en 2008. En 2006, il participe à la création de SolarCity, une société d'énergie solaire qui est ensuite acquise par Tesla et devient Tesla Energy. En 2015, il cofonde et copréside OpenAI, une association de recherche promouvant l'intelligence artificielle amicale, qu'il quitte en 2018. En 2016, il fonde The Boring Company, société de construction de tunnels, et Neuralink, société de neurotechnologie.
-
-À partir de 2018, Elon Musk a régulièrement des déboires avec la Securities and Exchange Commission (SEC), et est notamment accusé de manipuler la bourse, en usant de son influence, notamment sur les médias sociaux, dont Twitter. Ses actions et déclarations sont très médiatisées.
-"""
-
+name = "emmanuel macron"
 if __name__ == "__main__":
     print("Hello LangChain")
 
-    linkedin_profile_url = linkedin_lookup_agent(name="Thomas benard")
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_data = linkedin_profile_url
+    # linkedin_data = scrape_linkedin_profiles(linkedin_profile_url=linkedin_profile_url)
+
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username, num_tweets=5)
 
     summary_template = """
         given the information {information} about a person from I want you to create:
@@ -35,6 +33,5 @@ if __name__ == "__main__":
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = linkedin_profile_url
-    # linkedin_data = scrape_linkedin_profiles(linkedin_profile_url=linkedin_profile_url)
-    print(chain.run(information=linkedin_data))
+    print(chain.run(information=tweets))
+    print(tweets)
